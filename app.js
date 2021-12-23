92d2939c36176aadf67e9ae643466375bd4f5db3
Dmytro Tilug <tilugdmytro@gmail.com>
2021-12-23 11:00:18 +0200

    подключил базу данных

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const DB_HOST =
+  "mongodb+srv://tilugdmytro:z654321@cluster0.rd1ix.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("success");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
 
 const contactsRouter = require("./routes/api/contacts");
 
---
+++ mongodb-project/app.js
@@ -0,0 +1,13 @@
+// const mongoose = require("mongoose");
+// const DB_HOST =
+//   "mongodb+srv://tilugdmytro:z654321@cluster0.rd1ix.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+// mongoose
+//   .connect(DB_HOST)
+//   .then(() => {
+//     console.log("success");
+//   })
+//   .catch((error) => {
+//     console.log(error.message);
+//     process.exit(1);
+//   });
